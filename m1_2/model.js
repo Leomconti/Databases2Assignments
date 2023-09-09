1477387ee90de0eb89103b5abcd4a21d4dfeef8d
Anderson-Soares-Martins <andersoaresmartins@gmail.com>
2023-09-09 08:32:17 -0300

    inclusao de model

--- m1_2/model.js
+++ m1_2/model.js
@@ -3,11 +3,40 @@ const sequelize = require("./conn.js");
 
 class TableCsv extends Model {}
 TableCsv.init(
-    {},
-    {
-        sequelize,
-        modelName: "TableCsv",
-    }
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sex: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    phone: {
+      type: DataTypes.STRING,
+    },
+    date_of_birth: {
+      type: DataTypes.DATEONLY,
+    },
+    job_title: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    modelName: "TableCsv",
+  }
 );
 
 module.exports = TableCsv;
