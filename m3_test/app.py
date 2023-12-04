5fc7efe1ab5347b1787ad2e912fe44b4cb581e89
Leonardo Mosimann Conti <46422901+Leomconti@users.noreply.github.com>
2023-12-04 01:35:40 -0300

    fix: adjustments

--- m3_test/app.py
+++ m3_test/app.py
@@ -9,11 +9,11 @@ CORS(app)
 cassandra = get_cassandra_session()
 
 
-@app.route("/employees-by-manager/<manager_name>", methods=["GET"])
-def employees_by_manager(manager_name):
+@app.route("/employees-by-manager/<manager_emp_no>", methods=["GET"])
+def employees_by_manager(manager_emp_no):
     html = request.args.get("html", default="true") == "true"
-    query = "SELECT * FROM employees_by_manager WHERE manager_name = %s"
-    rows = cassandra.execute(query, [manager_name])
+    query = "SELECT * FROM employees_by_manager WHERE manager_emp_no = %s"
+    rows = cassandra.execute(query, [manager_emp_no])
 
     if html:
         # Return HTML
@@ -33,10 +33,12 @@ def employees_by_manager(manager_name):
         return jsonify(data_json)
 
 
-@app.route("/employees-by-dept/<dept_name>/<query_date>", methods=["GET"])
-def employees_by_dept(dept_name, query_date):
+@app.route("/employees-by-dept/<dept_name>/<date_range>", methods=["GET"])
+def employees_by_dept(dept_name, date_range):
     html = request.args.get("html", default="true") == "true"
-    query = f"SELECT * FROM employees_by_dept WHERE dept_name = {dept_name} AND query_date = {query_date}"
+    from_date = date_range.split(",")[0]
+    to_date = date_range.split(",")[1]
+    query = f"SELECT * FROM employees_by_dept WHERE dept_name = {dept_name} AND to_date = {to_date} AND from_date = {from_date}"
     rows = cassandra.execute(query)
 
     if html:
@@ -78,6 +80,34 @@ def avg_salary_by_dept(dept_name):
         # Return JSON
         data_json = [dict(row._asdict()) for row in rows]
         print(data_json)
+        return jsonify(data_json)
+
+
+@app.route("/show-all/<table_name>", methods=["GET"])
+def show_all(table_name):
+    html = request.args.get("html", default="true") == "true"
+    valid_tables = ['employees_by_manager', 'employees_by_dept', 'avg_salary_by_dept']
+    
+    if table_name not in valid_tables:
+        return "Invalid table name", 400
+
+    query = f"SELECT * FROM {table_name}"
+    rows = cassandra.execute(query)
+
+    if html:
+        # Return HTML
+        data_html = "<table>"
+        for row in rows:
+            data_html += (
+                "<tr>"
+                + "".join([f"<td>{getattr(row, col)}</td>" for col in row._fields])
+                + "</tr>"
+            )
+        data_html += "</table>"
+        return render_template_string(data_html)
+    else:
+        # Return JSON
+        data_json = [dict(row._asdict()) for row in rows]
         return jsonify(data_json)
 
 
